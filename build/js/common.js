17c7c3a74094ad570d13d0dc1964b187d45d9cff
Ex-Zy <serega1905@ukr.net>
2016-03-11 15:47:51 +0200

    upd

--- build/js/common.js
+++ build/js/common.js
@@ -14,4 +14,36 @@ $(document).ready(function() {
 		}, 500);
 		return false;
 	});
+	// social fixed
+	function stickySocial() {
+		if ($('.js-sticky').length) { 
+			var el = $('.js-sticky');
+			var stickyTop = $('.js-sticky').offset().top; 
+			var footerTop = $('.bottom').offset().top; 
+			var stickyHeight = $('.js-sticky').outerHeight();
+			// var limit = footerTop - stickyHeight;
+			var limit = footerTop + 215;
+			$(window).scroll(function(){ 
+
+				var windowTop = $(window).scrollTop();
+				if ($(window).width() >= 1024) {
+				
+					if (stickyTop < windowTop){
+						el.css({ position: 'fixed', top: 10 });
+						el.addClass('is-active');
+					}
+					else {
+						el.css('position','static');
+						el.removeClass('is-active');
+					}
+
+					if (limit < windowTop) {
+						var diff = limit - windowTop;
+						el.css({top: diff});
+					} 
+				} 
+			});
+		}
+	};
+	stickySocial();
 });
